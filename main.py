5550a8b8c7d70a6f863db1d685209345e1e90ffa
Hridaya Sharma <sharma.hridaya14@gmail.com>
2024-09-16 04:07:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
-
-
-
-
+import os
 from src.video.video_model import VideoModel as vm
 
 
 st.title("Video Face Processing App")
 st.write("Upload an MP4 video to process.")
-
 
 
 uploaded_file = st.file_uploader("Choose a video file", type=["mp4"])
@@ -19,6 +15,8 @@ if uploaded_file is not None:
     if uploaded_file.type == "video/mp4":
         
         video_path = "temp/uploaded_video.mp4"
+        os.makedirs(os.path.dirname(video_path), exist_ok=True)
+
         with open(video_path, mode="wb") as f:
             f.write(uploaded_file.read())
 
